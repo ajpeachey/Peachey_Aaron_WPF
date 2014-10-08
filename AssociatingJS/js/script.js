e6d22b72177d9d3b0ee458b3541f30ad74fcb4e0
ajpeachey <ajpeachey@fullsail.edu>
2014-10-07 21:25:07 -0700

    Arrays
    Showing understanding of Arrays

--- AssociatingJS/js/script.js
+++ AssociatingJS/js/script.js
@@ -8,6 +8,12 @@ var yearOfBirth = 1981; //declaring and defining on the same line
 
 var isStudent = false; //Boolean Variable
 
-var firstName = "Aaron"; 
+var firstName = "Aaron";
+
+//Arrays
+
+var muppetNames = ["Kermit", "Miss Piggy", "Gonzo"]; //0,1,2,
+
+muppetNames[1] = "Rizzo"; //change index #1 to Rizzo
 
 
