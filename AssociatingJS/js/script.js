a40e295b8b69924d7992bbdc4f8c58bd98c248b6
ajpeachey <ajpeachey@fullsail.edu>
2014-10-07 21:08:02 -0700

    Declaring var
    Commit to show understanding of declaring var

--- AssociatingJS/js/script.js
+++ AssociatingJS/js/script.js
@@ -1,1 +1,8 @@
-alert("This is in the js file");
+//Javascript
+var age; //declaration
+age = 33; //definition
+
+//storage container = value
+
+var yearOfBirth = 1981; //declaring and defining on the same line
+
