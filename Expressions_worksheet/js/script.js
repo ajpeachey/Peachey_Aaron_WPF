0dc85848a56d9033c27b3e8d954bea3689a2f059
ajpeachey <ajpeachey@fullsail.edu>
2014-10-09 15:45:08 -0700

    Testing my basic knowledge
    This was a test to see if I had the general concept down.

--- Expressions_worksheet/js/script.js
+++ Expressions_worksheet/js/script.js
@@ -4,3 +4,10 @@ October 9th, 2014
 Expression Worksheet
  */
 
+var width = 10; //The width of the rectangle
+var height = 6; // The height of the rectangle
+var result = width * height; //The result of multiplying width times height
+
+console.log(result); // Printing the answer to width times height
+
+// This was a test to see if I had the general concept
