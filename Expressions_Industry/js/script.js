2ad138800d43a5267d8ba99f0b8bc101ba7149af
ajpeachey <ajpeachey@fullsail.edu>
2014-10-09 17:51:34 -0700

    expressions industry
    bug in the code I am trying to figure out.

--- Expressions_Industry/js/script.js
+++ Expressions_Industry/js/script.js
@@ -7,10 +7,11 @@ var week1 = prompt("We are going to find out how many leads per day we average! 
 var week2 = prompt("How many leads in week 2?");
 var week3 = prompt("Week 3?");
 var week4 = prompt("and lastly week 4.");
-
+var workDays = prompt("How many work days were in this month?");
 var totalLeads = week1 + week2 + week3 + week4;
-var workDays = Prompt("How many work days were in this month?");
 
 var average = totalLeads / workDays;
 
 var result = "Over the last four weeks, you collected" + " " + totalLeads + " " + "leads. That is an average of" + " " + average + " " + "leads per day based on" + " " + workDays + " " + "working days.";
+
+alert(result);
