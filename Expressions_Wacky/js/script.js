b12991fe765ade0040bf75c0d40f1e73bd79c310
ajpeachey <ajpeachey@fullsail.edu>
2014-10-09 19:13:29 -0700

    Expressions Wacky - Complete code
    commit complete code for expressions wacky

--- Expressions_Wacky/js/script.js
+++ Expressions_Wacky/js/script.js
@@ -6,10 +6,19 @@ Expressions Wacky
 var items = prompt("How many items did you receive from Amazon.com?");
 var largeItems = prompt("How many large boxes?");
 var mediumItems = prompt("How many medium boxes?");
-var smallItems = prompt("How many small boxes?")
+var smallItems = prompt("How many small boxes?");
 
-var smBoxWeight = 2
-var medBoxWeight = 4
-var larBoxWeight = 6
+var smBoxWeight = 2;
+var medBoxWeight = 4;
+var larBoxWeight = 6;
 
 
+var totalSmWeight = smallItems * smBoxWeight;
+var totalMedWeight = mediumItems * medBoxWeight;
+var totalLarWeight = largeItems * larBoxWeight;
+
+var totalCardboardWeight = totalSmWeight + totalMedWeight + totalLarWeight;
+
+var result = "Oh boy, the recycle man is going to love you! You accumulated" + " " + totalCardboardWeight + "lbs of cardboard.";
+
+alert(result);
