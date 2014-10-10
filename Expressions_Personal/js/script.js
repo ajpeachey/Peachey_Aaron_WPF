b182257b3b2d8426901a44d4c145edfb5b56b41d
ajpeachey <ajpeachey@fullsail.edu>
2014-10-09 18:40:55 -0700

    Expressions Personal
    first commit for expressions personal code untested.

--- Expressions_Personal/js/script.js
+++ Expressions_Personal/js/script.js
@@ -1,3 +1,13 @@
-/**
- * Created by AaronPeachey on 10/9/14.
+/* Aaron Peachey
+October 9th, 2014
+Expressions Personal
  */
+
+
+var totalTuition = prompt("Today we are going to determine how much you will owe in student loans. Please enter your total tuition here.");
+var grants = prompt("What is the total amount you will receive in grants?");
+var otherContributions = prompt("Will there be any money other than grants and loans to pay for school? What is that amount? Enter 0 if zero.");
+
+var result = totalTuition - grants - otherContributions
+
+var toPrint = "Your total student loan debt will be" + " " + result
