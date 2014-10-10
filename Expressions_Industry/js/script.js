35a591269e9ba71e20d7c06f3262a7bafa36d6ec
ajpeachey <ajpeachey@fullsail.edu>
2014-10-09 17:56:46 -0700

    expressions industry
    still trying to debug code.

--- Expressions_Industry/js/script.js
+++ Expressions_Industry/js/script.js
@@ -9,9 +9,7 @@ var week3 = prompt("Week 3?");
 var week4 = prompt("and lastly week 4.");
 var workDays = prompt("How many work days were in this month?");
 var totalLeads = week1 + week2 + week3 + week4;
-
 var average = totalLeads / workDays;
-
 var result = "Over the last four weeks, you collected" + " " + totalLeads + " " + "leads. That is an average of" + " " + average + " " + "leads per day based on" + " " + workDays + " " + "working days.";
 
 alert(result);
