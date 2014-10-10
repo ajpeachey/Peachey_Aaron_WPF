df71636aa4748151a09baff818ae6a2bd30d9cac
ajpeachey <ajpeachey@fullsail.edu>
2014-10-09 19:05:58 -0700

    Wacky Expressions - progress
    Progress commit for wacky expressions

--- Expressions_Wacky/js/script.js
+++ Expressions_Wacky/js/script.js
@@ -4,4 +4,12 @@ Expressions Wacky
  */
 
 var items = prompt("How many items did you receive from Amazon.com?");
-var cardboardWeight = 2
+var largeItems = prompt("How many large boxes?");
+var mediumItems = prompt("How many medium boxes?");
+var smallItems = prompt("How many small boxes?")
+
+var smBoxWeight = 2
+var medBoxWeight = 4
+var larBoxWeight = 6
+
+
