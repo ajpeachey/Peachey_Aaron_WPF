2b7d9f7b704503b8e7e3d7512dc77fc9fa462558
ajpeachey <ajpeachey@fullsail.edu>
2014-10-09 17:48:47 -0700

    Expressions industry
    first code attempt for expressions industry calculator

--- Expressions_Industry/js/script.js
+++ Expressions_Industry/js/script.js
@@ -1,3 +1,16 @@
-/**
- * Created by AaronPeachey on 10/9/14.
+/* Aaron Peachey
+October 9th, 2014
+Expressions Industry
  */
+
+var week1 = prompt("We are going to find out how many leads per day we average! Please tell me how many leads you had in week one of this month.");
+var week2 = prompt("How many leads in week 2?");
+var week3 = prompt("Week 3?");
+var week4 = prompt("and lastly week 4.");
+
+var totalLeads = week1 + week2 + week3 + week4;
+var workDays = Prompt("How many work days were in this month?");
+
+var average = totalLeads / workDays;
+
+var result = "Over the last four weeks, you collected" + " " + totalLeads + " " + "leads. That is an average of" + " " + average + " " + "leads per day based on" + " " + workDays + " " + "working days.";
