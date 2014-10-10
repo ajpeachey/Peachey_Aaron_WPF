9ce030a5fcfddd9d3efdd31416e7df7ade944759
ajpeachey <ajpeachey@fullsail.edu>
2014-10-09 18:29:43 -0700

    Expressions Industry commit with notes.
    Final Expressions industry commit with notes.

--- Expressions_Industry/js/script.js
+++ Expressions_Industry/js/script.js
@@ -3,13 +3,13 @@ October 9th, 2014
 Expressions Industry
  */
 
-var week1 = +prompt("We are going to find out how many leads per day we average! Please tell me how many leads you had in week one of this month.");
-var week2 = +prompt("How many leads in week 2?");
-var week3 = +prompt("Week 3?");
-var week4 = +prompt("and lastly week 4.");
-var workDays = prompt("How many work days were in this month?");
-var totalLeads = week1 + week2 + week3 + week4;
-var average = totalLeads / workDays;
+var week1 = +prompt("We are going to find out how many leads per day we average! Please tell me how many leads you had in week one of this month."); //first prompt for addition formula
+var week2 = +prompt("How many leads in week 2?"); //second prompt for addition formula
+var week3 = +prompt("Week 3?"); //third prompt for addition formula
+var week4 = +prompt("and lastly week 4."); //forth prompt for addition formula
+var workDays = prompt("How many work days were in this month?"); //prompt for days worked in a month
+var totalLeads = week1 + week2 + week3 + week4; //formula adding total leads over 4 weeks
+var average = totalLeads / workDays; //determining average per day
 var result = "Over the last four weeks, you collected" + " " + totalLeads + " " + "leads. That is an average of" + " " + average + " " + "leads per day based on" + " " + workDays + " " + "working days.";
-
+//print result for final alert
 alert(result);
