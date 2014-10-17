6cae50d04ec0039fb7a8aef4996715f9493f9f5a
ajpeachey <ajpeachey@fullsail.edu>
2014-10-16 18:51:24 -0700

    code not working
    debugging code, committing prior version

--- Conditional_Personal/js/script.js
+++ Conditional_Personal/js/script.js
@@ -14,6 +14,10 @@ var hoursPerDay = Number(hoursSlept) / 7
 
 if(hoursSlept >= hoursSleepNeeded){
     alert("You are getting" + " " + hoursPerDay + " " + "hours per night of sleep. That amount will help keep you healthy and give you" + " " + hoursLeftOver + " " + "hours of free time each week!" )
+
+    if(hoursSlept == ""){
+        Prompt("Ooops! The program won't work if you don't give me an answer! Please enter the number of hours you slept last night.")
+    }
 }else{
     alert("You should really be getting more sleep! Right now you have" + " " + hoursLeftOver + " " + "hours of free time each week, you have time to spare.")
 }
