1885b233ee4af7c1fac02bbf45212ec4b9f9b6f5
ajpeachey <ajpeachey@fullsail.edu>
2014-10-16 17:59:33 -0700

    progress
    progress on conditional worksheet

--- Conditionals_worksheet/js/script.js
+++ Conditionals_worksheet/js/script.js
@@ -8,5 +8,11 @@
 
 var mpg = 14
 var percentTank = 20
-var tankCap = 12
+var tankCap = 25
+var gallonsLeft = tankCap / percentTank
+var distance = gallonsLeft * mpg
 
+
+if(distance > 200){
+    console.log("Yes, you can make it without stopping for gas!")
+}else console.log("You only have" " " gallonsLeft " " "gallons of gas in your tank, better get gas now while you can!")
