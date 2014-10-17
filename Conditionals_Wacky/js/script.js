eff52adb6a1b8500b1a4479523c580b26c2fd496
ajpeachey <ajpeachey@fullsail.edu>
2014-10-16 20:12:00 -0700

    commit
    progress

--- Conditionals_Wacky/js/script.js
+++ Conditionals_Wacky/js/script.js
@@ -1,3 +1,11 @@
 /**
- * Created by AaronPeachey on 10/16/14.
+ Conditionals Wacky
+ Aaron Peachey
+ 10/16/2014
  */
+
+var hasNuts = prompt("Do you like nuts in your candy bar?");
+var size = prompt("Do prefer regular or king size?");
+
+(hasNuts == "yes") ? alert("Sometimes you feel like a nut!") : alert("Sometimes you don't!");
+(size == "king" || "king size") ? alert("That's a big candy bar!") : alert("Sometimes regular is just right!");
