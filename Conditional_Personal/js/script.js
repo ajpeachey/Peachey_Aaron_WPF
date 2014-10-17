6e1d1a1cf3e25d8da8e96a43917cc6ae6211fb6b
ajpeachey <ajpeachey@fullsail.edu>
2014-10-16 18:56:02 -0700

    getting closer
    still buggy. working on code to figure it out

--- Conditional_Personal/js/script.js
+++ Conditional_Personal/js/script.js
@@ -12,12 +12,11 @@ var hoursLeftOver = hourInWeek - (Number(hoursSlept) + Number(hoursWorked))
 var hoursSleepNeeded = 56
 var hoursPerDay = Number(hoursSlept) / 7
 
+if(hoursSlept == "") {
+    prompt("Ooops! The program won't work if you don't give me an answer! Please enter the number of hours you slept last night.")
+}
 if(hoursSlept >= hoursSleepNeeded){
     alert("You are getting" + " " + hoursPerDay + " " + "hours per night of sleep. That amount will help keep you healthy and give you" + " " + hoursLeftOver + " " + "hours of free time each week!" )
-
-    if(hoursSlept == ""){
-        Prompt("Ooops! The program won't work if you don't give me an answer! Please enter the number of hours you slept last night.")
-    }
 }else{
     alert("You should really be getting more sleep! Right now you have" + " " + hoursLeftOver + " " + "hours of free time each week, you have time to spare.")
 }
