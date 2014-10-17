e9ed56ae6d88ce30da384017e10f360d58bc6a17
ajpeachey <ajpeachey@fullsail.edu>
2014-10-16 19:51:02 -0700

    industry conditional with logical operator
    complete

--- Conditionals_Industry/js/script.js
+++ Conditionals_Industry/js/script.js
@@ -4,3 +4,13 @@
  10/16/2014
  */
 
+var understanding = prompt("You are completely understanding everything in this class. True or False?")
+var expert = prompt("You feel that you will reach expert level by the time you are done with this class. True or False?")
+
+if(understanding && expert == "True"){
+    alert("You will move on to a bright future in web design and development!")
+}else{
+    alert("You probably are going to waste $57,000 and not amount to much!")
+
+
+}
