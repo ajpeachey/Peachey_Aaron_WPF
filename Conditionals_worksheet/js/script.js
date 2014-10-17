b4df2b2a1c03f6b35f691f2ef3423bad0747c7a0
ajpeachey <ajpeachey@fullsail.edu>
2014-10-16 18:08:12 -0700

    Changes in code sample
    made adjustments in my math

--- Conditionals_worksheet/js/script.js
+++ Conditionals_worksheet/js/script.js
@@ -7,12 +7,14 @@
 //Last Chance For Gas!
 
 var mpg = 14
-var percentTank = 20
+var percentTank = .25 //percent of tank left in decimal form
 var tankCap = 25
-var gallonsLeft = tankCap / percentTank
+var gallonsLeft = tankCap * percentTank
 var distance = gallonsLeft * mpg
 
 
-if(distance > 200){
+if(distance >= 200){
     console.log("Yes, you can make it without stopping for gas!")
-}else console.log("You only have" " " gallonsLeft " " "gallons of gas in your tank, better get gas now while you can!")
+}else {
+    console.log("You only have" + " " + gallonsLeft + " " + "gallons of gas in your tank, better get gas now while you can!")
+}
