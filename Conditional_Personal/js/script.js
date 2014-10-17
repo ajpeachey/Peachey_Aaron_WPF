8e7a5dbd9c5313af839554e9f07b5123bc8f8a36
ajpeachey <ajpeachey@fullsail.edu>
2014-10-16 18:45:54 -0700

    first commit of code
    commit to show progress on conditionals personal

--- Conditional_Personal/js/script.js
+++ Conditional_Personal/js/script.js
@@ -3,3 +3,17 @@
  Aaron Peachey
  10/16/2014
  */
+
+
+var hoursSlept = prompt("How many hours of sleep did you get last week?")
+var hoursWorked = prompt("How many hours did you work last week?")
+var hourInWeek = 168
+var hoursLeftOver = hourInWeek - (Number(hoursSlept) + Number(hoursWorked))
+var hoursSleepNeeded = 56
+var hoursPerDay = Number(hoursSlept) / 7
+
+if(hoursSlept >= hoursSleepNeeded){
+    alert("You are getting" + " " + hoursPerDay + " " + "hours per night of sleep. That amount will help keep you healthy and give you" + " " + hoursLeftOver + " " + "hours of free time each week!" )
+}else{
+    alert("You should really be getting more sleep! Right now you have" + " " + hoursLeftOver + " " + "hours of free time each week, you have time to spare.")
+}
