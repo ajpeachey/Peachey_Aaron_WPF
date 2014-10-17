dd29e4bed1a1df4e91a2f8e5d9ba6240a198555c
ajpeachey <ajpeachey@fullsail.edu>
2014-10-16 20:26:37 -0700

    buggy code.
    Can’t figure out how to make it work properly. Need to spend more time
    on it

--- Conditionals_Wacky/js/script.js
+++ Conditionals_Wacky/js/script.js
@@ -5,7 +5,9 @@
  */
 
 var hasNuts = prompt("Do you like nuts in your candy bar?");
-var size = prompt("Do prefer regular or king size?");
 
 (hasNuts == "yes") ? alert("Sometimes you feel like a nut!") : alert("Sometimes you don't!");
+var size = prompt("Do prefer regular or king size?");
+
 (size == "king" || "king size") ? alert("That's a big candy bar!") : alert("Sometimes regular is just right!");
+(hasNuts == "yes" && size == "King Size" || "King") ? alert("We recommend a King Size Almond Joy!") : alert("We think a Regular Size Mounds is your style");
