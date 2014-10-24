cf0d608e6521c97a1c42bcff0f6caf8d2e7b9e47
ajpeachey <ajpeachey@fullsail.edu>
2014-10-23 17:31:47 -0700

    functions worksheet
    final

--- Functions_Worksheet/js/script.js
+++ Functions_Worksheet/js/script.js
@@ -1,5 +1,5 @@
 /**
- Expressions Worksheet
+ Functions Worksheet
  Aaron Peachey
  10/23/14
  */
@@ -7,7 +7,12 @@
 
 //Stung
 
-var bodyWeight = 150
-var stingPound = 8.666666667
+var bodyWeight = 150 // fixed variable for weight of person
+var stingPound = 8.666666667 // number of stings per pound needed to kill someone
 
+function beeSting(bodyWeight, stingPound){ //Basic function setup
+    var toKill = bodyWeight * stingPound
+    console.log("It takes" + " " + toKill + " " + " bee stings to kill this animal.")
+}
 
+beeSting(bodyWeight, stingPound)
