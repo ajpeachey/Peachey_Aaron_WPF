1a2569be03c86124a80aa24884ff364b17cb4895
ajpeachey <ajpeachey@fullsail.edu>
2014-10-23 17:55:06 -0700

    Functions Industry
    Complete

--- Functions_Industry/js/script.js
+++ Functions_Industry/js/script.js
@@ -1,3 +1,19 @@
 /**
- * Created by AaronPeachey on 10/23/14.
+ Functions Industry
+ Aaron Peachey
+ 10/23/2014
  */
+
+
+var baseFee = +prompt("What is the base fee for this site?");
+var hoursWorked = +prompt("How many hours estimated for this project?")
+var hourlyRate = 150
+
+
+function siteFees(b, hw, hr){
+var feeCalc = b + hw * hr
+var total = "The total fee for this website will be $" + feeCalc + "."
+    alert(total)
+};
+
+siteFees(baseFee,hoursWorked,hourlyRate)''
