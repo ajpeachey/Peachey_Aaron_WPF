cb805c8b0b9bfc000b07b26f2fa553b888b4dcee
ajpeachey <ajpeachey@fullsail.edu>
2014-10-23 18:09:52 -0700

    Functions Personal
    file set up and start of functions personal

--- Functions_Personal/js/script.js
+++ Functions_Personal/js/script.js
@@ -1,3 +1,17 @@
 /**
- * Created by AaronPeachey on 10/23/14.
+ Functions Personal
+ Aaron Peachey
+ 10/23/14
  */
+
+var item = prompt("Today we are looking to buy a new car. Would you like to continue to see if it is possible?")
+
+if(item == "yes"){
+    alert("Great! Lets continue!")
+}else{
+    alert("That's too bad, I guess our game is over.")
+}
+
+
+var bank = prompt("How much money is in your bank account right now?");
+var cash = prompt("How much cash do you have on hand?")
