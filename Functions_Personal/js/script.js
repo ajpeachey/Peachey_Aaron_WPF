b1d846d282e63085a1d632250043e28e1be66929
ajpeachey <ajpeachey@fullsail.edu>
2014-10-23 18:18:46 -0700

    testing calculations
    testing the calculations of code

--- Functions_Personal/js/script.js
+++ Functions_Personal/js/script.js
@@ -7,11 +7,15 @@
 var item = prompt("Today we are looking to buy a new car. Would you like to continue to see if it is possible?")
 
 if(item == "yes"){
-    alert("Great! Lets continue!")
+    carcalc()
 }else{
     alert("That's too bad, I guess our game is over.")
 }
 
-
-var bank = prompt("How much money is in your bank account right now?");
-var cash = prompt("How much cash do you have on hand?")
+function carcalc() {
+    var carCost = +prompt("How much does the new car cost?")
+    var bank = +prompt("How much money is in your bank account right now?");
+    var cash = +prompt("How much cash do you have on hand?")
+    var percent = carCost * .20
+console.log(percent)
+}
