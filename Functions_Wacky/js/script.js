e7b89ff6893dfd5dea6b2641143c7a343563d2e7
ajpeachey <ajpeachey@fullsail.edu>
2014-10-23 19:24:17 -0700

    expressions wacky
    final code with comments

--- Functions_Wacky/js/script.js
+++ Functions_Wacky/js/script.js
@@ -7,19 +7,19 @@
 
 var peanutsOnHand = +prompt("Today we need to find out if the zoo has enough peanuts to get through the week. How many pounds of peanuts are on hand right now?");
 var peanutBackUp = +prompt("How many pounds are in the peanut reserves?");
-var daysLeft = +prompt("How many days until the next peanut shipment?")
-var poundsElephantEats = 200 * daysLeft
+var daysLeft = +prompt("How many days until the next peanut shipment?");
+var poundsElephantEats = 200 * daysLeft;
 
-function peanutCalc(peanutsOnHand, peanutBackUp, daysLeft){
+function peanutCalc(peanutsOnHand, peanutBackUp, daysLeft){ //function setup with 3 parameters
     var survive = peanutsOnHand + peanutBackUp / daysLeft
     return survive;
 }
 
-peanutCalc(peanutsOnHand, peanutBackUp, daysLeft)
+peanutCalc(peanutsOnHand, peanutBackUp, daysLeft); //calling the function
 
 
 if(peanutCalc(peanutsOnHand, peanutBackUp, daysLeft) >= poundsElephantEats || peanutsOnHand >= poundsElephantEats || peanutBackUp >= poundsElephantEats){
     alert("Looks like you have enough peanuts for the elephants to survuve!")
 }else{
     alert("Watch out! Those elephants are going to get hungry, and they have to eat something!")
-}
+};// if statement
